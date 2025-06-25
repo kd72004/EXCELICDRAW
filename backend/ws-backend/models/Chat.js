3c610317c684ccc9869b88f83a30ccd4864e9dbc
Kalyani Dave <kalyanidave2004@gmail.com>
2025-06-25 22:37:33 +0530

    final changes

--- backend/ws-backend/models/Chat.js
+++ backend/ws-backend/models/Chat.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
   message: { type: String, required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  roomId: { type: String, required: true },
+  roomId: { type: String, required: true }, // just store roomId string
+}, {
   timestamps: true 
 });
 
